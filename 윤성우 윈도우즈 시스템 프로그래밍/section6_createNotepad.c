d53be74baa55960b183a039a02827a94b50b877c
juyeon Lee <76759160+leejuyeon-star@users.noreply.github.com>
2022-07-27 16:46:18 +0900

    Update section6_createNotepad.c

--- 윤성우 윈도우즈 시스템 프로그래밍/section6_createNotepad.c
+++ 윤성우 윈도우즈 시스템 프로그래밍/section6_createNotepad.c
@@ -11,7 +11,15 @@ int _tmain(int argc, TCHAR* argv[]){
     STARTUPINFO si = {0, };     //STARTUPINFO 구조체 초기화
     PROCESS_INFORMATION pi;      //PROCESS_INFOMATION 구조체 선언
 
-    si.cb = sizeof(si);     //STARTUPINFO 구조체 변수의 크기
+    //STARTUPINFO 구조체 속성 지정
+    si.cb = sizeof(si);     //STARTUPINFO 구조체 변수의 크기. 업그레이드 버전과 그 전 버전을 구분하기 위함
+    //si.dwFlags=STARTF_USEPOSITION|STARTF_USESIZE;	//si 구조체 변수에 설정된 값중 반영하려 하는 멤버들의 정보
+    //si.dwX=100;		//화면 좌상단 기준으로 값만큼 떨어진 위치에서 프로세스 출력
+    //si.dwY=200;		//화면 좌상단 기준으로 값만큼 떨어진 위치에서 프로세스 출력
+    //si.dwXSize=300;		//size 설정
+    //si.dwYSize=200;		//size 설정
+    // si.lpTitle=_T("I am a boy!");	//콘솔 윈도우의 타이틀 바 제목
+	
     TCHAR command[] = _T("notepad.exe");       //실행할 파일명 or 완전경로
     TCHAR cDir[DIR_LEN];       //메모리 버퍼
     BOOL state;
