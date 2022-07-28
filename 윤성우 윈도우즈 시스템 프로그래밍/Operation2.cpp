54e1d84b49a5c786192d23b969a7e2c40e1f64c4
juyeon Lee <76759160+leejuyeon-star@users.noreply.github.com>
2022-07-28 21:48:37 +0900

    Add files via upload

---
+++ 윤성우 윈도우즈 시스템 프로그래밍/Operation2.cpp
@@ -0,0 +1,26 @@
+// 예제 6-2
+// 부모 프로세스의 handle table을 이용하여 자식 프로세스의 우선순위 변경하기
+
+
+#include <stdio.h>
+#include <windows.h>
+#include <tchar.h>
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	// 우선순위 변경하는 함수
+	// 인수 : (우선순위를 변경할 리소스의 핸들, 우선순위 정보)
+	// GetCurrentProcess() : 현재 실행되고 있는 프로세스를 반환. (고정된 특정값이 반환됨)
+	// HIGH_PRIORITY_CLASS : 우선순위를 가장 위로 올림
+	SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);		
+
+	while(1)
+	{
+		for(DWORD i=0; i<10000; i++)
+			for(DWORD i=0; i<10000; i++); //Busy Waiting!!
+
+		_fputts(_T("Operation2.exe \n"), stdout); 
+	}
+
+	return 0;
+}
