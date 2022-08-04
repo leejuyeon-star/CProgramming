34da9078dd272f1d65b074071107afd2752b21c2
juyeon Lee <76759160+leejuyeon-star@users.noreply.github.com>
2022-08-04 23:22:38 +0900

    Update MailReceiver.cpp

--- 윤성우 윈도우즈 시스템 프로그래밍/MailReceiver.cpp
+++ 윤성우 윈도우즈 시스템 프로그래밍/MailReceiver.cpp
@@ -1,7 +1,7 @@
 /*
 	예제 7-2
-	mailslot 기반의 IPC
-	mailslot으로 한 프로세스가 다른 프로세스에게 문자열 받는 프로세스 만들기
+	메일슬롯 방식의 IPC
+	메일 슬롯으로 한 프로세스에게서 문자열 받는 프로세스 만들기
 	MailReceiver.cpp
 	
 */
@@ -18,12 +18,38 @@ int _tmain(int argc, LPTSTR argv[])
 	TCHAR messageBox[50];
   	DWORD bytesRead;  // number of bytes read
 
+	/*************함수 정보********************
+	CreateMailslotW(
+		_In_     LPCWSTR lpName,		//슬롯 이름(주소 지정)
+		_In_     DWORD nMaxMessageSize,		//buffer 크기 (0일경우 최대크기로 설정됨)
+		_In_     DWORD lReadTimeout,			//ReadFile 함수의 블로킹 여부(MAILSLOT_WAIT_FOREVER 인 경우 블로킹 상태, 0인 경우 블로킹x)
+		_In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes			//핸들 상속(다른 자식프로세스에게 공개하는 것) 여부를 결정짓는 구조체의 주소
+		);
+
+	// LPSECURITY_ATTRIBUTES 정보
+		typedef struct _SECURITY_ATTRIBUTES {
+		DWORD nLength;				//구조체 크기
+		LPVOID lpSecurityDescriptor;		
+		BOOL bInheritHandle;			// 해당 자식 프로세스 핸들을 상속(다른 자식프로세스에게 공개하는 것)할지 결정지음(true는 상속 허락)
+	} SECURITY_ATTRIBUTES, *PSECURITY_ATTRIBUTES, *LPSECURITY_ATTRIBUTES;
+
+
+	********************************************************/
+
+
+	// @@@ 해당 자식 프로세스 핸들을 상속(다른 자식프로세스에게 공개하는 것)을 허락할 시 아래 코드 삽입
+	// SECURITY_ATTRIBUTES sa;
+	// sa.nLength = sizeof(sa);
+	// sa.lpSecurityDescriptor = NULL;
+	// sa.bInheritHandle = TRUE;
+
+
 	/* mailslot(우체통) 생성 */
 	hMailSlot=CreateMailslot(
-			SLOT_NAME, 	//슬롯 이름
-			0, 		//buffer 크기(우체통 크기) (0일경우 최대크기로 설정됨)
-			MAILSLOT_WAIT_FOREVER, 	//ReadFile 함수 특성
-			NULL	//보안
+			SLOT_NAME, 	
+			0, 		
+			MAILSLOT_WAIT_FOREVER, 	
+			NULL
 			);
 
 	if(hMailSlot==INVALID_HANDLE_VALUE)
@@ -58,6 +84,54 @@ int _tmain(int argc, LPTSTR argv[])
 	}
 
 	CloseHandle(hMailSlot);
+
+
+//*********************************************************
+#define SLOT_NAME2 _T("\\\\.\\mailslot\\mailbox2")
+	HANDLE hMailSlot2;  //mailslot 핸들
+	TCHAR message[50];
+    DWORD bytesWritten;  // number of bytes write
+
+	hMailSlot2=CreateFile(		//전달할 파일 마련
+		//모두 고정
+		SLOT_NAME2, 		//연결할 상대 슬롯 이름
+		GENERIC_WRITE, 		//메일 슬롯 용도(읽기,쓰기,읽기/쓰기)
+		FILE_SHARE_READ, 
+		NULL,
+		OPEN_EXISTING, 		//파일의 생성 방식(새로운 파일 생성 or 기존 파일에 접근)
+		FILE_ATTRIBUTE_NORMAL, 
+		NULL);
+
+	if(hMailSlot2==INVALID_HANDLE_VALUE)
+	{
+		_fputts(_T("Unable to create mailslot!!\n"), stdout);
+		return 1;
+	}
+
+	while(1)
+	{
+		_fputts(_T("MY CMD>"), stdout);
+		_fgetts(message, sizeof(message)/sizeof(TCHAR), stdin);
+
+		if(!WriteFile(		//데이터 보내는 함수
+			hMailSlot2, 	//연결할 파일 핸들
+			message, 	//전송 데이터 버퍼(전송할 데이터 담겨있음)
+			_tcslen(message)*sizeof(TCHAR), 	//전송 크기
+			&bytesWritten, 	//전송 결과
+			NULL))	
+		{
+			_fputts(_T("Unable to write!"), stdout);
+			CloseHandle(hMailSlot2);		//현 프로세스의 handle table에서 해당 프로세스의 핸들을 삭제하는 함수
+			return 1;
+		}
+
+		if(!_tcscmp(message, _T("exit")))
+		{
+			_fputts(_T("Good Bye!"), stdout);
+			break;
+		}
+
+	}
 
 	return 0;
 }
