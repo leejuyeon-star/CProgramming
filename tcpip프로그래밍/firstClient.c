16f4615a2098e12135f927e7ed100c3f3ccdb850
juyeon Lee <76759160+leejuyeon-star@users.noreply.github.com>
2022-08-04 14:25:07 +0900

    Update firstClient.c

--- tcpip프로그래밍/firstClient.c
+++ tcpip프로그래밍/firstClient.c
@@ -2,11 +2,11 @@
 #include <Winsock2.h>
 #include <WS2tcpip.h>
 
-// 클라이언트 소스
+// 클라이언트 소스 - 서버와 채팅 주고받기
 // 참고 : https://cafe.naver.com/gamepromaster
 
 
-//**************GCC(MinGW)로 컴파일시*******************
+//**************@@@ GCC(MinGW)로 컴파일시*******************
 // pragma는 GCC에서 유효하지 않으므로 무시됨. 또한 MinGW에는 "ws2_32.lib"이 없으므로 문제 발생.
 // 따라서 아래 명령어로 컴파일해야 함
 //> gcc 컴파일할 파일명 -o 생성될 파일명 -lws2_32
@@ -19,7 +19,6 @@ int main(void){
     WSADATA wsaData;
     SOCKET hSocket;
     SOCKADDR_IN servAddr;
-    char *ip_address="127.0.0.1";   //서버의 ip주소
 
     // 1. 윈속 초기화
     if(WSAStartup(MAKEWORD(2,2), &wsaData) != 0){
@@ -29,9 +28,9 @@ int main(void){
 
     // 2. socket 생성
     hSocket = socket(
-        AF_INET,    //IPv4 버전을 의미
-        SOCK_STREAM,    //TCP를 의미 (UDP는 SOCK_DGRAM)
-        IPPROTO_TCP     //TCP를 의미 (UDP는 IPPROTO_UDP)
+        AF_INET,        //@@@ IP 버전 설정 (IPv4 버전은 AF_INET, IPv6은 AF_INET6)
+        SOCK_STREAM,    //@@@ (TCP는 SOCK_STREAM, UDP는 SOCK_DGRAM)
+        IPPROTO_TCP     //@@@ (TCP는 IPPROTO_TCP, UDP는 IPPROTO_UDP)
         );
     if(hSocket == INVALID_SOCKET) {
         printf("Faild socket() \n");
@@ -42,14 +41,15 @@ int main(void){
     // 3. 서버 연결
     //      3-1. 연결할 서버의 IP주소, port번호 지정
     memset(&servAddr, 0, sizeof(servAddr));
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_port = htons(30002);       //서버의 port 번호 설정 (htons() : 호스트의 엔디안으로 데이터 변환)
+    servAddr.sin_family = AF_INET;  //@@@ IP 버전 설정 (IPv4 버전은 AF_INET, IPv6은 AF_INET6)
+    char *ip_address="127.0.0.1";   //@@@ 서버의 ip주소 설정
+    servAddr.sin_port = htons(30002);       //@@@ 서버의 port 번호 설정 (htons() : 호스트의 엔디안으로 데이터 변환)
 
-    //      3-1. ip 주소를 binary 형태로 변환
+    //      3-1. @@@ ip 주소를 binary 형태로 변환 (아래 두가지 방법 중 한가지 선택)
     //***************Mingw32를 사용하지 않는 경우**************************
         //inet_pton() 사용
     // inet_pton(
-    //     AF_INET,        //IPv4 버전을 의미
+    //     AF_INET,        //@@@ IP 버전 설정 (IPv4 버전은 AF_INET, IPv6은 AF_INET6)
     //     (LPSTR)ip_address,    //ip 주소
     //     &servAddr.sin_addr  //ip주소를 저장할 곳
     //     );
@@ -60,7 +60,7 @@ int main(void){
     int size_sock_addr_in = sizeof(servAddr);
     WSAStringToAddress(
         (LPSTR)ip_address,  //ip 주소
-        AF_INET,    //IPv4 버전을 의미
+        AF_INET,    //@@@ IP 버전 설정 (IPv4 버전은 AF_INET, IPv6은 AF_INET6)
         NULL, 
         (LPSOCKADDR)&servAddr, 
         (LPINT)&size_sock_addr_in
@@ -121,5 +121,3 @@ int main(void){
     system("pause");    // 실행되자마자 곧바로 꺼지지 않도록 일시정지
     return 0;
 }
-
-
