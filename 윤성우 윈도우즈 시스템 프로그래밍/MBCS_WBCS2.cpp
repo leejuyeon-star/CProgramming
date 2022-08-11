48afeeef3ed9fd5e831fc5487f16d47de5424cd7
juyeon Lee <76759160+leejuyeon-star@users.noreply.github.com>
2022-08-11 17:05:47 +0900

    Update MBCS_WBCS2.cpp

--- 윤성우 윈도우즈 시스템 프로그래밍/MBCS_WBCS2.cpp
+++ 윤성우 윈도우즈 시스템 프로그래밍/MBCS_WBCS2.cpp
@@ -4,19 +4,14 @@
 	* 자료형 사용시 동시지원하는 자료형 매크로를 사용해야 함. windows.h를 참조.
 	* 문자열 사용시 _T("문자열") 형식으로 작성해야 함
 	* 함수 사용시 동시지원하는 함수 매크로를 사용해야 함. tchar.h를 참조.
-
-
 	******************************* windows.h에서 제공하는 동시지원 자료형 ***********************************
 	typedef char	CHAR;
 	typedef wchar_t	WCHAR;
-
 	#define CONST const
 	typedef CHAR *		LPSTR
 	typedef CONST CHAR *	LPCSTR;
-
 	typedef WCHAR *		LPWSTR;
 	typedef CONST WCHAR *	LPCWSTR;	
-
 	******************************** windows.h에서 제공하는 자료형 관련 매크로 ************************************
 	* UNICODE, _UNICODE의 정의여부를 확인 후 동시지원 자료형과 형식을 그에 맞는 자료형과 형식으로 바꿔준다
 	#ifdef UNICODE
@@ -27,13 +22,11 @@
 		typedef CHAR	TCHAR;
 		typedef LPSTR	LPTSTR;
 		typedef LPCSTR	LPCTSTR;
-
 	* 문자열 사용시 _T("문자열") 형식으로 작성해야 하는 이유
 	#ifdef _UNICODE
 		#define __T(x)	L ## x		//WBCS인 경우 문자열 앞에 L붙여야 하기 때문
 	#else
 		#define __T(x)	x
-
 	#define _T(x)	__T(x)			//_T(x) == __T(x) == _TEXT(x)
 	#define _TEXT(x)	__T(x)	
 	//*******************************************************************************	
@@ -43,30 +36,35 @@
 
 
 //***********************문자 셋 변환하는 방법***************************
-// * WBCS(UNICODE) 기반으로 컴파일하고자 하는 경우 아래 코드 삽입
-// #define UNICODE
-// #define _UNICODE
+// * WBCS(UNICODE) 기반으로 컴파일하고자 하는 경우 "#include보다 위에" 해당 코드 삽입
+// * 이 경우 _tmain 불가. main만 가능
+#define UNICODE
+#define _UNICODE
 
 // * MBCS 기반으로 컴파일하고자 하는 경우 아래 코드 삽입
-#undef UNICODE
-#undef _UNICODE
+// #undef UNICODE
+// #undef _UNICODE
 //**************************************************
 
 
 
-#include <stdio.h>
-#include <tchar.h>		//동시지원하는 함수를 MBCS, WBCS 함수로 변환해줌. 함수 사용시 이 파일에서 함수 검색.
 #include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>		//동시지원하는 함수를 MBCS, WBCS 함수로 변환해줌. 함수 사용시 이 파일에서 함수 검색.
 #include <locale.h>		//WBCS 한글 사용시 필요	
 
-int _tmain(int argc, TCHAR* argv[])		// main을 _tmain으로 변환
-{
-	//WBCS 한글 사용시 필요
-	_wsetlocale(LC_ALL, L"korean");		
+// int _tmain(int argc, TCHAR* argv[])	//이유는 모르겠으나 WBCS인 경우 _tmain 사용시 오류발생
+int main(void)		//WBCS인 경우 main 사용(MBCS도 사용해도 무관함)
+{	
+	// @@@ WBCS 한글 사용시 적용, (MBCS인 경우 반드시 주석처리)
+	_wsetlocale(LC_ALL, L"korean");	
 
 	//문자열의 sizeof, len 확인	
 	TCHAR str1[] = _T("abcde");
 	TCHAR str2[] = _T("가나다라마");
+
+	_tprintf(_T("%s \n"), str2);
 
 	_tprintf(_T("string size: %d \n"), sizeof(str1));		//메모리 할당 크기 출력
 	_tprintf(_T("string length: %d \n"), _tcslen(str1));	//문자열의 길이 출력(문자의 개수 출력)
@@ -76,7 +74,7 @@ int _tmain(int argc, TCHAR* argv[])		// main을 _tmain으로 변환
 
 
 
-	//********입력한 두 문자열 결합하기************
+	// //********입력한 두 문자열 결합하기************
 	TCHAR str3[100];
 	TCHAR str4[50];
 	_fputts(_T("Input String 1 : "), stdout);	
@@ -86,8 +84,7 @@ int _tmain(int argc, TCHAR* argv[])		// main을 _tmain으로 변환
 
 	_tcscat(str3, str4);
 	_tprintf(_T("String1 + String2 : %s \n"), str3);
-	//************************************
+	// //************************************
 
 	return 0;
 }
-
